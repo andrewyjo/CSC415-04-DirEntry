589fadfda17fca16b46f28f8fc9fdf605b66b2eb
andrewyjo <andrewyunejo@gmail.com>
2022-09-28 14:26:00 -0700

    changes

--- DirEntry.c
+++ DirEntry.c
@@ -1,25 +1,37 @@
 /**************************************************************
-* Class:  CSC-415-0# Fall 2022
+* Class:  CSC-415-03 Fall 2022
 * Name: Andy Cho, 
-* Student ID: 920484081
-* GitHub Name: andrewyjo
-* Project: Assignment # – 
+* Student ID: 920484081, 
+* GitHub Name: andrewyjo,
+* Project: Group Assignment - File System - Directory Entry Prototype
 *
 * File: DirEntry.c
 *
-* Description: 
+* Description: An early prototype of the Boberts Directory Entry.
+* To test: gcc test.c; ./a.out;
+* 
 *
 **************************************************************/
-
+#include <time.h>
 
 struct DirEntry{
-        //pointer / actual data
-        //additional variables
 
-        char name[45];
-        int addr;
+        char name[255];
+        //Combined with the filename, the path can be upto 4096bytes.
+        char path[4096];
         char type[25];
-        int size;
+        char owner[25];
+
+        //permissions: "----------" string with 'r,w,x' modifiers
+        char permissions[11];
+        unsigned int groupID;
+        unsigned int nameLength;
+        unsigned int pathLength;
+        unsigned int id;
+        unsigned int addr;
+        unsigned int size;
+        time_t lastAccess;
+        time_t lastMod;
 
 
-}
+};
---
+++ test.c
@@ -0,0 +1,40 @@
+/**************************************************************
+* Class:  CSC-415-03 Fall 2022
+* Name: Andy Cho,
+* Student ID: 920484081,
+* GitHub Name: andrewyjo,
+* Project: Group Project - Dir Entry - Test Program
+*
+* File: test.c
+*
+* Description: Run instructions: gcc test.c; ./a.out;
+*
+**************************************************************/
+#include "DirEntry.c"
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+int test(){
+
+        struct DirEntry de;
+        strcpy(de.name, "pizza.jpeg");
+        strcpy(de.path, "/root/pizza.jpeg");
+        strcpy(de.type, "file");
+        strcpy(de.owner, "boberts");
+        strcpy(de.permissions, "-rwxr-xr-x");
+        de.groupID = 1;
+        de.nameLength = 10;
+        de.pathLength = 16;
+        de.id = 0;
+        de.addr = 2;
+        de.lastAccess = 0;
+        de.lastMod = 0;
+
+        printf("\n••••••Testing••••••\n");
+        printf("\n•name:%s\n•path:%s\n•type:%s•owner:%s\n•permissions:%s\n•groupID: %d\n•nameLength: %d\n•pathLength:%d\n•id:%d\n•addr:%d\n•lastAccess:%ld, lastMod:%ld\n", de.name,  de.path, de.type, de.owner, de.permissions, de.groupID, de.nameLength, de.pathLength, de.id, de.addr, de.lastAccess, de.lastMod);
+         return 0;
+
+}
+
+
