ed853e1c0c5e50772eb60648fbb46f9ecb5b4bab
andrewyjo <andrewyunejo@gmail.com>
2022-09-28 12:06:25 -0700

    initialization

---
+++ DirEntry.c
@@ -0,0 +1,25 @@
+/**************************************************************
+* Class:  CSC-415-0# Fall 2022
+* Name: Andy Cho, 
+* Student ID: 920484081
+* GitHub Name: andrewyjo
+* Project: Assignment # – 
+*
+* File: DirEntry.c
+*
+* Description: 
+*
+**************************************************************/
+
+
+struct DirEntry{
+        //pointer / actual data
+        //additional variables
+
+        char name[45];
+        int addr;
+        char type[25];
+        int size;
+
+
+}
